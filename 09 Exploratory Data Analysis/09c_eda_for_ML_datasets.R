3c0b6aa73fa3b4485878837c00e236e2b0b1aec0
MarinFotache <marinfotache@gmail.com>
2019-12-05 14:20:03 +0200

    Update 09c_eda_for_ML_datasets.R

--- 09 Exploratory Data Analysis/09c_eda_for_ML_datasets.R
+++ 09 Exploratory Data Analysis/09c_eda_for_ML_datasets.R
@@ -260,6 +260,32 @@ ggplot(., aes(x = Value, y = Murder)) +
      theme(strip.text.x = element_text(size = 12)) +
      xlab("")  
 
+
+
+#####################################################################
+### 	                      I.2 Insurance                          ###
+#####################################################################
+### data available on 
+### https://github.com/stedy/Machine-Learning-with-R-datasets
+
+## Variables
+## `age`: age of primary beneficiary
+## `sex`: insurance contractor gender, female, male
+## `bmi`: Body mass index, providing an understanding of body, 
+##   weights that are relatively high or low relative to height, 
+##   objective index of body weight (kg / m ^ 2) using the ratio 
+##   of height to weight, ideally 18.5 to 24.9
+## `children`: Number of children covered by health insurance / Number 
+##   of dependents
+## `smoker`: Smoking
+## `region`: the beneficiary's residential area in the US, northeast, southeast, 
+##        southwest, northwest.
+## `charges`: Individual medical costs billed by health insurance
+insurance <- readr::read_csv('insurance.csv')
+
+##
+## to be completed during the labs!
+##
  
 #####################################################################
 ###	     II. EDA for datasets to be used for classification      ###	
